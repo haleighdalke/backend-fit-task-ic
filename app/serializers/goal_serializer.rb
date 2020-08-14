da0db4d5bb08e62664a95a7bc6970b0d709f9db3
darrickpang <dkypang@yahoo.com>
2020-08-14 13:40:55 -0700

    backend should be good now -Darrick

--- app/serializers/goal_serializer.rb
+++ app/serializers/goal_serializer.rb
@@ -1,4 +1,4 @@
 class GoalSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :frequency, :duration, :duration_type, :accomplishments
+  attributes :frequency, :duration, :duration_type, :accomplishments, :user, :habit
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@ User.create(name: 'Darrick Pang', password: '123', age: 30, location: 'Californi
 Habit.create(activity: 'running', activity_type: 'cardio')
 Goal.create(user_id: 1, habit_id: 1, frequency: 'one per day', duration: 2, duration_type: 'miles')
 Accomplishment.create(goal_id: 1, date: 'August 13, 2020', duration: 2, duration_type: 'miles')
+
